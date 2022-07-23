43596a5cb503b3664bfbace52b6a6ce77af2df6c
Jjeffrey94 <98789106+Jjeffrey94@users.noreply.github.com>
2022-07-22 19:08:18 -0500

    correcion

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -9,12 +9,12 @@ describe("Buy a t-shirt", () => {
     cy.get(".cart_navigation span").click();
     cy.get("#email").type("aperdomobo@gmail.com");
     cy.get("#passwd").type("WorkshopProtractor");
-    cy.get(".login").click();
-    cy.get(".cart_navigation > .button > span");
-    cy.get("#cgv");
-    cy.get(".cart_navigation > .button > span");
-    cy.get(".bankwire");
-    cy.get("#cart_navigation > .button > span");
+    cy.get("#SubmitLogin").click();
+    cy.get(".cart_navigation > .button > span").contains("Proceed to checkout").click();
+    cy.get("#cgv").click();
+    cy.get(".cart_navigation > .button > span").contains("Proceed to checkout").click();
+    cy.get(".bankwire").contains("Pay by bank wire").click();
+    cy.get("#cart_navigation > .button > span").contains("I confirm my order").click();
     cy.get("#center_column > div > p > strong").should(
         "have.text",
         "Your order on My Store is complete.",
