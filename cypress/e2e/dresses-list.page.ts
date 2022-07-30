b0e7f2cd80b4414c2a4d2d9f335bad430bd35983
Jjeffrey94 <98789106+Jjeffrey94@users.noreply.github.com>
2022-07-29 19:40:24 -0500

    correciones

--- cypress/e2e/dresses-list.page.ts
+++ cypress/e2e/dresses-list.page.ts
@@ -1,15 +1,20 @@
 import { MenuContentPage, DressesListPage } from "../page/index";
 
 describe("the user navigates to the dresses page should", () => {
+  let names: string[];
   let menuContentPage: MenuContentPage;
   let dressesListPage: DressesListPage;
 
   before(() => {
     menuContentPage = new MenuContentPage();
     dressesListPage = new DressesListPage();
+    names = ["Printed Chiffon Dress", "Printed Summer Dress", "Printed Dress", "Printed Dress", "Printed Summer Dress"];
   });
 
   it("show the available dresses", () => {
-    // ... realiza la prueba
+    menuContentPage.visitMenuContentPage();
+    menuContentPage.visitDressMenu();
+    dressesListPage.validateItemsNumber(5);
+    dressesListPage.validateItemsNames(names);
   });
 });
